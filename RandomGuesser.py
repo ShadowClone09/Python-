952dadbdb0ef76bcd9f4469eef5564eeab4f2e9f
Rhythm Aich <48093060+ShadowClone09@users.noreply.github.com>
2020-01-07 00:22:55 +0530

    A guessing game in Python

---
+++ RandomGuesser.py
@@ -0,0 +1,76 @@
+
+#Guess a random number
+
+import random
+
+u = int(input("Set a range. Guess from "))
+
+v = int(input(" to "))
+
+a = random.randint(u, v)
+
+print("I have guessed a number.")
+
+n = int(input("How many chances do you want?"))
+
+print("Guess the number now.")
+
+i = 0
+
+while i < n:
+
+    i = i + 1
+
+    b = int(input())
+
+    if b == a:
+
+        print("Correct!!")
+
+        print("The number was " + str(a), ".")
+
+        break
+
+    elif b - a > 2:
+
+        print("Too high!!")
+
+
+    elif b - a < -2:
+
+        print("Too low!!")
+
+
+    elif b - a <= 2:
+
+        print("Almost there...")
+
+
+    elif b - a <= -2:
+
+        print("Almost there...")
+
+    if i == n:
+
+        print("You're out of chances!")
+        break
+
+p = i - 1
+
+#if p > 0:
+    #print("The number was " + str(a))
+
+q = 10 - p
+
+print("You scored " + str(q), "points.")
+
+
+
+
+
+
+
+
+
+
+
